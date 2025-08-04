e3ee2ff6ca14174aca777b5d7dabc8ed5345331b
George Zubrienko <gzu@ecco.com>
2025-08-04 12:09:22 +0200

    Add support for Scylla Cql Store (#14)

--- app/app_dependencies.go
+++ app/app_dependencies.go
@@ -6,22 +6,30 @@ import (
 	"github.com/SneaksAndData/nexus-supervisor/services"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/tools/record"
 	"k8s.io/klog/v2"
 )
 
 type ApplicationServices struct {
-	cqlStore   *request.CqlStore
-	recorder   record.EventRecorder
+	cqlStore *request.CqlStore
+	// recorder   record.EventRecorder
 	kubeClient *kubernetes.Clientset
 	// TODO: Nexus API client also required for loading template definitions (for exit codes etc.)
 	supervisor *services.Supervisor
 }
 
-func (appServices *ApplicationServices) WithCqlStore(ctx context.Context, bundleConfig *request.AstraBundleConfig) *ApplicationServices {
+func (appServices *ApplicationServices) WithAstraCqlStore(ctx context.Context, bundleConfig *request.AstraBundleConfig) *ApplicationServices {
 	if appServices.cqlStore == nil {
 		logger := klog.FromContext(ctx)
 		appServices.cqlStore = request.NewAstraCqlStore(logger, bundleConfig)
+	}
+
+	return appServices
+}
+
+func (appServices *ApplicationServices) WithScyllaCqlStore(ctx context.Context, config *request.ScyllaCqlStoreConfig) *ApplicationServices {
+	if appServices.cqlStore == nil {
+		logger := klog.FromContext(ctx)
+		appServices.cqlStore = request.NewScyllaCqlStore(logger, config)
 	}
 
 	return appServices
